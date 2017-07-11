5304e3029660cb5131f4451b4658170e1d730d8b
Juan <jcamejob@gmail.com>
2017-07-11 19:09:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -59,8 +59,10 @@ backup({
 */
 
 // view engine setup
-app.set('views', path.join(__dirname , 'views'));
+
+app.set('views', './app/views');
 app.set('view engine', 'ejs');
+
 
 
 app.use(logger('dev'));
